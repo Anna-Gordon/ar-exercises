276f9b21941ae60949176670ad4954854315e5dc
Anna Gordon <annetta@email.com>
2020-03-31 00:15:01 +0000

    employees seeds

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+
+total_revenue = Store.sum(:annual_revenue)
+puts total_revenue
+average_annual_revenue = Store.average(:annual_revenue)
+puts average_annual_revenue
+stores = Store.where("annual_revenue > ?", 1000000).size
+puts stores
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,16 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Theresa", last_name: "Huber", hourly_rate: 60)
+@store1.employees.create(first_name: "Ruben", last_name: "Lin", hourly_rate: 65)
+@store1.employees.create(first_name: "Keshawn", last_name: "Lam", hourly_rate: 50)
+@store1.employees.create(first_name: "Addison", last_name: "Sims", hourly_rate: 40)
+@store1.employees.create(first_name: "Justine", last_name: "Rush", hourly_rate: 55)
+
+@store2.employees.create(first_name: "Lincoln", last_name: "Jennings", hourly_rate: 35)
+@store2.employees.create(first_name: "Ada", last_name: "Kramer", hourly_rate: 40)
+@store2.employees.create(first_name: "Donte", last_name: "Cardenas", hourly_rate: 50)
+@store2.employees.create(first_name: "Nikolas", last_name: "Skinner", hourly_rate: 53)
+@store2.employees.create(first_name: "Paloma", last_name: "Guzman", hourly_rate: 47)
+
+# puts employees
